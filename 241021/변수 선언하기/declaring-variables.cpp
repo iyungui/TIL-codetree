e64575172d1860bece14db7ed8ac5520cfbb1c26
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-21 02:16:26 +0900

    Title: 변수 선언하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241021/변수 선언하기/declaring-variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    char b;
+    a = 3, b = 'C';
+    cout << a << endl;
+    cout << b;
+    return 0;
+}
