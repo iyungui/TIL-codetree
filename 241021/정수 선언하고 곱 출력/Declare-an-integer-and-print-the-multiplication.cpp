b95afebde8a0ac3cc8eae0dc81a7a4650de520cd
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-21 02:18:57 +0900

    Title: 정수 선언하고 곱 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241021/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
+++ 241021/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b, c;
-    a = 26, b = 5, c = a * b;
-    cout << a << " * " << b << " = " << c;
+    int a, b;
+    a = 26, b = 5;
+    cout << a << " * " << b << " = " << a * b;
     return 0;
 }
