828d423b28792b4244a025f14f72c1259e5cb0a9
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-21 23:59:24 +0900

    Title: 살 수 있는 물건, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241021/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    if (n >= 3000) {
+        cout << "book";
+    } else if {
+        cout << "mask";
+    } else {
+        cout << "no";
+    }
+    return 0; 
+}
