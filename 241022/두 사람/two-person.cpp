c44736db4bf969451076abbc5affee85076b5a2c
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-22 15:52:22 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241022/두 사람/two-person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a_age, b_age;
+    char a_gender, b_gender;
+    cin >> a_age >> a_gender >> b_age >> b_gender;
+    cout << ((a_gender == 'M') || (b_gender == 'M')) && ((a_age >= 19) || (b_age >= 19));
+    return 0;
+}
