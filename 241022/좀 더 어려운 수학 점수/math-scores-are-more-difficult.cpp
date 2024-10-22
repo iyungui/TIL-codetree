887bd492f138b512eb61388d6ecd17c8f1f42cb2
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-22 15:48:50 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 241022/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -7,16 +7,22 @@ int main() {
     cin >> a_math >> a_eng;
     cin >> b_math >> b_eng;
 
-    if (a_math > b_math) {
-        cout << 'A';
-    } else if (a_math < b_math) {
-        cout << 'B';
+    // if (a_math > b_math) {
+    //     cout << 'A';
+    // } else if (a_math < b_math) {
+    //     cout << 'B';
+    // } else {
+    //     if (a_eng > b_eng) {
+    //         cout << 'A';
+    //     } else {
+    //         cout << 'B';
+    //     }
+    // }
+
+    if ((a_math > b_math) || ((a_math == b_math) && (a_eng > b_eng))) {
+        cout << "A";
     } else {
-        if (a_eng > b_eng) {
-            cout << 'A';
-        } else {
-            cout << 'B';
-        }
+        cout << "B";
     }
 
 
