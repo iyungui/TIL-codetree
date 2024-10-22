d0a3ce58d27040126417ecbbc3bb45d774024243
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-22 12:39:35 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241022/점수 비교/score-comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+
+    cout << (a > c && b > d);
+    return 0;
+}
