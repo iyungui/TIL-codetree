1bd9dabf4926c75d01d0cdef88b30982cff2d918
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-23 10:32:55 +0900

    Title: 최댓값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/최댓값 구하기/maximum-value.cpp
+++ 241023/최댓값 구하기/maximum-value.cpp
@@ -6,6 +6,20 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c;
     cin >> a >> b >> c;
-    cout << max({a, b, c});
+    // cout << max({a, b, c});
+
+    if (a >= b) {
+        if (a >= c) {
+            cout << a;
+        } else {
+            cout << c;
+        }
+    } else {
+        if (b >= c) {
+            cout << b;
+        } else {
+            cout << c;
+        }
+    }
     return 0;
 }
