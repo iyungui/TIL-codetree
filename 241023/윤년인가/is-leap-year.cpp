7b5d6694920008dbdcdc12c48592cd390152b9a5
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-23 10:02:52 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241023/윤년인가/is-leap-year.cpp
+++ 241023/윤년인가/is-leap-year.cpp
@@ -5,10 +5,13 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int y;
     cin >> y;
-    if (y % 4 != 0 && (y % 100 == 0 && y % 400 != 0)) {
-        cout << "false";
-    } else {
+    if (y % 4 == 0) {
+        if (y % 100 == 0 && y % 400 != 0) {
+            cout << "false";
+        }
         cout << "true";
+    } else {
+        cout << "false";
     }
     return 0;
 }
