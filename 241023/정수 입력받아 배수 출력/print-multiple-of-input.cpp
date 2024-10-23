7cdd293727a857fd0868c9afddf7879715e627f4
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-23 22:56:08 +0900

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/정수 입력받아 배수 출력/print-multiple-of-input.cpp
+++ 241023/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -6,11 +6,16 @@ int main() {
     int n;
     cin >> n;
     int count = 0;
-    for (int i = n; count < 5; i++) {
-        if (i % n == 0) {
-            cout << i << " ";
-            count++;
-        }
+    // for (int i = n; count < 5; i++) {
+    //     if (i % n == 0) {
+    //         cout << i << " ";
+    //         count++;
+    //     }
+    // }
+
+    for (int i = n; count < 5; i += n) {
+        cout << i << " ";
+        count++;
     }
     return 0;
 }
