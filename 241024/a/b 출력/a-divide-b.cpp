72d2a4107f05654ec47c8f9ce0033f4c20eccbc0
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-24 23:30:21 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241024/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a / b << ".";
+
+    a %= b;
+    for (int i = 0; i < 20; i++) {
+        a *= 10;
+        cout << a / b;
+        a %= b;
+    }
+    return 0;
+}
