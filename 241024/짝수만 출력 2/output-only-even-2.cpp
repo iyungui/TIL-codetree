346fc95da0910b8b378424046fc296ede26a88a0
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-24 10:33:25 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241024/짝수만 출력 2/output-only-even-2.cpp
+++ 241024/짝수만 출력 2/output-only-even-2.cpp
@@ -5,10 +5,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int b, a;
     cin >> b >> a;
-    int i = a;
-    while (i <= b) {
+    int i = b;
+    while (i >= a) {
         cout << i << " ";
-        i += 2;
+        i -= 2;
     }
     return 0;
 }
