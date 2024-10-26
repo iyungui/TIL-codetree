9c6bc1a0e917ab38066d163af34277543b0a45c5
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-26 19:14:02 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/총합 구하기/find-the-total.cpp
+++ 241026/총합 구하기/find-the-total.cpp
@@ -5,6 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     int sum = 0;
+    cin >> a >> b;
     for (int i = a; i <= b; i++) {
         if (i % 6 == 0 && i % 8 != 0) {
             sum += i;
