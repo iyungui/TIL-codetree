8969eebd52b195053cc11d11077b9c56db790b21
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-26 18:11:59 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/완전수 판별기/perfect-number-discriminator.cpp
+++ 241026/완전수 판별기/perfect-number-discriminator.cpp
@@ -5,7 +5,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     int sum = 0;
-    for (int i = 1; i <= n; i++) {
+    cin >> n;
+    for (int i = 1; i < n; i++) {
         if (n % i == 0) {
             sum += i;
         }
