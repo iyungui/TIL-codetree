e83969fca7aaacc8696dc81215afd04b4b34798e
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-26 16:53:45 +0900

    Title: 숫자 사이의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/숫자 사이의 배수/multiplication-between-numbers.cpp
+++ 241026/숫자 사이의 배수/multiplication-between-numbers.cpp
@@ -5,12 +5,17 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     cin >> a >> b;
-    int sum_val = 0;
+    int sum_val = 0, cnt = 0;
+    
     for (int i = a; i <= b; i++) {
         if (i % 5 == 0 || i % 7 == 0) {
             sum_val += i;
+            cnt++;
         }
     }
-    cout << sum_val << " " << sum_val / b - a;
+    cout << sum_val << " ";
+    cout << fixed;
+    cout.precision(1);
+    cout << (double)sum_val / cnt;
     return 0;
 }
