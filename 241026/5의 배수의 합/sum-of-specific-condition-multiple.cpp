fd1a35814b58a730bb08bd6ae7d3428ebfa33457
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-26 17:24:09 +0900

    Title: 5의 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/5의 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 241026/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -6,9 +6,17 @@ int main() {
     int a, b;
     cin >> a >> b;
     int sum_val = 0;
-    for (int i = a; i <= b; i++) {
-        if (i % 5 == 0) {
-            sum_val += i;
+    if (a <= b) {
+        for (int i = a; i <= b; i++) {
+            if (i % 5 == 0) {
+                sum_val += i;
+            }
+        }
+    } else {
+        for (int i = b; i <= a; i++) {
+            if (i % 5 == 0) {
+                sum_val += i;
+            }
         }
     }
     cout << sum_val;
