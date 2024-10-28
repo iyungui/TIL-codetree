9d532c52fd84f849d19aee4978d7522ddef3c5fe
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-28 13:48:40 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241028/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    bool satisfied = false;
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            satisfied = true;
+        }
+    }
+    if (satisfied == true) {
+        cout << 'C';
+    } else {
+        cout << 'N';
+    }
+    return 0;
+}
