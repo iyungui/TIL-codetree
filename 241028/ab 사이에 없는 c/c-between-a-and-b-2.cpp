b34ec6c70437e1fd5a061e0ef9e8505004fc2054
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-28 23:51:15 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241028/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    bool satisfied = true;
+    for (int i = a; i <= b; i++) {
+        if (i % c == 0) {
+            satisfied = false;
+        }
+    }
+    if (satisfied) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    return 0;
+}
