9cafb7fad0e3d3212fcfbdb12e9aebe2c50cad59
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-29 00:01:00 +0900

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241029/피보나치 수/fibonacci-number.cpp
+++ 241029/피보나치 수/fibonacci-number.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 
 int fibonacci(int n) {
-    if (n <= 1)
-        return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    int a = 0, b = 1, c;
+    if (n == 0)
+        return a;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
 
 int main() {
