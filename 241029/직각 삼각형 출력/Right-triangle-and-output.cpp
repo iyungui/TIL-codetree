f30f53b0c7cb44ffd78157d901b6b90fde8ced48
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-29 11:42:57 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241029/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 241029/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -5,11 +5,17 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i ; j++) {
-            cout << '*';
-        }
-        for (int k = 1; k < i; k++) {
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= i ; j++) {
+    //         cout << '*';
+    //     }
+    //     for (int k = 1; k < i; k++) {
+    //         cout << '*';
+    //     }
+    //     cout << endl;
+    // }
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= 2 * i + 1; j++) {
             cout << '*';
         }
         cout << endl;
