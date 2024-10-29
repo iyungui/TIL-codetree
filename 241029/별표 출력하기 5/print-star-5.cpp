19cbce6a656d9ce8d8d2c163af3c992176a2968f
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-29 11:17:27 +0900

    Title: 별표 출력하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241029/별표 출력하기 5/print-star-5.cpp
+++ 241029/별표 출력하기 5/print-star-5.cpp
@@ -5,15 +5,24 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    // for (int i = 1; i <= n; i++) {
 
-        for (int j = n; j >= i; j--) {
-            for (int k = n; k >= i; k--) {
+    //     for (int j = n; j >= i; j--) {
+    //         for (int k = n; k >= i; k--) {
+    //             cout << '*';
+    //         }
+    //         cout << " ";
+    //     }
+
+    //     cout << endl;
+    // }
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            for (int k = 1; k <= i; k++) {
                 cout << '*';
             }
             cout << " ";
         }
-
         cout << endl;
     }
     return 0;
