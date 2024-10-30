143e252874667a2ef239d0df51d5011d06661316
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-30 22:58:07 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/별표 출력하기 8/print-star-8.cpp
+++ 241030/별표 출력하기 8/print-star-8.cpp
@@ -5,12 +5,12 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        if (i % 2 == 1)
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0)
             cout << '*';
         else {
-            for (int j = 0; j < i; j++)
-                cout << '*' << ' ';
+            for (int j = 0; j < i + 1; j++)
+                cout << "* ";
         }
         cout << endl;
     }
