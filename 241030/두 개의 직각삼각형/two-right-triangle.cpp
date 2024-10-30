c9942bafae85b53e4fcb82c96ef09ac9b7bd5781
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-30 14:40:41 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/두 개의 직각삼각형/two-right-triangle.cpp
+++ 241030/두 개의 직각삼각형/two-right-triangle.cpp
@@ -6,15 +6,30 @@ int main() {
     int n;
     cin >> n;
 
+    // 내 풀이
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n - i; j++) {
+    //         cout << "*";
+    //     }
+    //     for (int j = 0; j < i; j++) {
+    //         cout << " ";
+    //     }
+
+    //     for (int j = 0; j < i; j++) {
+    //         cout << " ";
+    //     }
+    //     for (int j = 0; j < n - i; j++) {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // main
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - i; j++) {
             cout << "*";
         }
-        for (int j = 0; j < i; j++) {
-            cout << " ";
-        }
-
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < 2 * i; j++) {
             cout << " ";
         }
         for (int j = 0; j < n - i; j++) {
