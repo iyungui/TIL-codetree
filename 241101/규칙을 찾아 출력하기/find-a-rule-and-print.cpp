fdc5b61dacd0cad913bc4618d881a011c45c02b9
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-01 01:29:28 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241101/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 241101/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -5,22 +5,34 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cout << "* ";
-    }
-    cout << endl;
+    // 내 풀이
+    // for (int i = 0; i < n; i++) {
+    //     cout << "* ";
+    // }
+    // cout << endl;
 
-    for (int i = 1; i < n - 1; i++) {
-        for (int j = 0; j < i; j++)
-            cout << "* ";
-        for (int j = 0; j < n - i - 1; j++)
-            cout << "  ";
-        cout << "* ";
+    // for (int i = 1; i < n - 1; i++) {
+    //     for (int j = 0; j < i; j++)
+    //         cout << "* ";
+    //     for (int j = 0; j < n - i - 1; j++)
+    //         cout << "  ";
+    //     cout << "* ";
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < n; i++) {
+    //     cout << "* ";
+    // }
+
+    // 해설 풀이
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i > j || i == 0 || i == n - 1 || j == 0 || j == n - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
         cout << endl;
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << "* ";
     }
     return 0;
 }
