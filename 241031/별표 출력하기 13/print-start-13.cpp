a3d90baaf4dcfc780c40d04e38bcd07ca3646ff7
이융의 <121233350+iyungui@users.noreply.github.com>
2024-10-31 12:17:39 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241031/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i = 0; i < 2 * n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < n - (i / 2); j++)
+                cout << "* ";
+            cout << endl;
+        } else {
+            for (int j = 0; j < (i / 2) + 1; j++)
+                cout << "* ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
