3e0052d2095fb214a687179dbea88d5b3d761663
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-01 14:20:17 +0900

    Title: 홀수로 이루어진 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241101/홀수로 이루어진 정사각형/square-with-odd-numbers.cpp
+++ 241101/홀수로 이루어진 정사각형/square-with-odd-numbers.cpp
@@ -5,15 +5,22 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    int sum = 11;
-    for (int i = 0; i < n; i++) {
+    // int sum = 11;
+    // for (int i = 0; i < n; i++) {
         
+    //     for (int j = 0; j < n; j++) {
+    //         cout << sum << " ";
+    //         sum += 2;
+    //     }
+    //     for (int j = 0; j < n - 1; j++) {
+    //         sum -= 2;
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cout << sum << " ";
-            sum += 2;
-        }
-        for (int j = 0; j < n - 1; j++) {
-            sum -= 2;
+            cout << i * 2 + j * 2 + 11 << " ";
         }
         cout << endl;
     }
