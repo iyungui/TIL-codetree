41f28acc3b30a906a655c9411a03d0ea2390e8cc
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-01 15:51:44 +0900

    Title: 좌우 지그재그 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241101/좌우 지그재그 출력/print-lr-zigzag.cpp
+++ 241101/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -6,16 +6,28 @@ int main() {
     int n;
     cin >> n;
     int cnt = 0;
+    // 내 풀이
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         if (i % 2 == 0) {
+    //             cout << ++cnt << " ";
+                
+    //         } else {
+    //             cout << cnt-- << " ";
+    //         }
+    //     }
+    //     cnt += n;
+    //     cout << endl;
+    // }
+
+    // 해설 풀이
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (i % 2 == 0) {
-                cout << ++cnt << " ";
-                
-            } else {
-                cout << cnt-- << " ";
-            }
+            if (i % 2 == 0)
+                cout << (i * n) + j + 1 << " ";
+            else
+                cout << (i * n) + n - j << " ";
         }
-        cnt += n;
         cout << endl;
     }
     return 0;
