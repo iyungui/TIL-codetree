7ce98f849ba89018a8d502cdab65b02e80eec7f7
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-02 12:23:18 +0900

    Title: 숫자 피라미드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241102/숫자 피라미드/number-pyramid.cpp
+++ 241102/숫자 피라미드/number-pyramid.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i + 1; j++) {
-            cout << i << " ";
+            cout << i + 1 << " ";
         }
         cout << endl;
     }
