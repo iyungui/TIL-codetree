50f344a72fdb69eed0c2b3fc8c83eb052f604788
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-04 09:40:22 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/10개 입력받기/receive-10-inputs.cpp
+++ 241104/10개 입력받기/receive-10-inputs.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int sum = 0, cnt = 0;
+    int sum = 0, cnt = 10;
     int arr[10];
     for(int i = 0; i < 10; i++) {
         cin >> arr[i];
