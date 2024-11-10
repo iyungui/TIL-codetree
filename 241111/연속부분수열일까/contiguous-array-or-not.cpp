7014ba0c01032e4809d037d05c7db7940d50fa3f
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-11 01:48:53 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241111/연속부분수열일까/contiguous-array-or-not.cpp
+++ 241111/연속부분수열일까/contiguous-array-or-not.cpp
@@ -13,21 +13,50 @@ int main() {
     for (int i = 0; i < n2; i++) {
         cin >> b_arr[i];
     }
-    
+
+    int idx = 0;
+    int cnt = 0;
     bool isPart = false;
-    int idx = 0, cnt = 0;
-    for (int i = 0; i < n2; i++) {
-        for (int j = idx; j < n1; j++) {
-            if (a_arr[j] == b_arr[i]) {
+
+    for (int i = 0; i < n1; i++) {
+        // if (idx == n2 - 1 && i < n1) idx = 0
+
+        for (int j = idx; j < n2; j++) {
+            if (a_arr[i] == b_arr[j]) {
                 cnt++;
                 idx = j + 1;
                 break;
+            } else {
+                cnt = 0;
+                idx = 0;
             }
-            else cnt = 0;
         }
-        if (cnt == n2) isPart = true;
+        if (cnt == n2) {
+            isPart = true;
+            break;
+        }
     }
     if (isPart) cout << "Yes";
     else cout << "No";
+    
+    // bool isPart = false;
+    // int idx = 0;
+    // int cnt = 0;
+    // for (int i = 0; i < n2; i++) {
+    //     for (int j = idx; j < n1; j++) {
+    //         if (a_arr[j] == b_arr[i]) {
+    //             cnt++;
+    //             idx = j + 1;
+    //             // cout << "idx: " << idx << endl;
+    //             break;
+    //         }
+    //         else cnt = 0;
+    //     }
+    //     cout << i << " cnt: " << cnt << endl;
+    //     if (cnt == n2) isPart = true;
+    // }
+    // if (isPart) cout << "Yes";
+    // else cout << "No";
+
     return 0;
 }
