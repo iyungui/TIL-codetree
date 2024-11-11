0d2e9b1154ab74d2678fe1e9dc0ba82b9e2fad2b
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-11 13:11:19 +0900

    Title: n개의 숫자 중 최소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241111/n개의 숫자 중 최소/min-of-n-num.cpp
+++ 241111/n개의 숫자 중 최소/min-of-n-num.cpp
@@ -7,21 +7,21 @@ int main() {
     int n;
     cin >> n;
 
-    int arr[101] = {};
-    int count_arr[101] = {};
-    int min_val = INT_MAX;
+    int arr[100] = {};
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        count_arr[arr[i]]++;
     }
-
-    for (int i = 1; i <= n; i++) {
+    int min_val = arr[0];
+    for (int i = 0; i < n; i++) {
         if (min_val > arr[i]) {
             min_val = arr[i];
         }
-        // cout << "count_arr: " << count_arr[i] << endl;
     }
-    cout << min_val << " " << count_arr[min_val];
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (min_val == arr[i]) cnt++;
+    }
+    cout << min_val << " " << cnt;
     return 0;
 }
