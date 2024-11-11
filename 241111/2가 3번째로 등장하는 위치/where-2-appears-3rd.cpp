ecf9d79dc908f9cff4749e64577d527eaef78906
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-11 12:36:59 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241111/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int cnt = 0;
+    int arr[100];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (arr[i] == 2) cnt++;
+        if (cnt == 3) {
+            cout << i + 1;
+            break;
+        }
+    }
+    return 0;
+}
