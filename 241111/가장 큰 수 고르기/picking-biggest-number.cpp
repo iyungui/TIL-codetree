f088b211d77d32cde5a821b45bea7f35b3ae7826
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-11 12:44:26 +0900

    Title: 가장 큰 수 고르기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241111/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int max_val = INT_MIN;
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if (max_val < arr[i]) max_val = arr[i];
+    }
+    cout << max_val;
+    return 0;
+}
