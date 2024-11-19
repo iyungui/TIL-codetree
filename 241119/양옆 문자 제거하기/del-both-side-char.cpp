9530b64c8178a00927a44896bd34ff54863eeb8f
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-19 22:27:29 +0900

    Title: 양옆 문자 제거하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241119/양옆 문자 제거하기/del-both-side-char.cpp
+++ 241119/양옆 문자 제거하기/del-both-side-char.cpp
@@ -6,6 +6,12 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string str;
     cin >> str;
-    cout << str.substr(0, 1) + str.substr(2, str.length() - 4) + str.substr(str.length() - 1);
+    // solution: substr
+    // cout << str.substr(0, 1) + str.substr(2, str.length() - 4) + str.substr(str.length() - 1);
+    
+    // solution: erase
+    str.erase(1, 1);
+    str.erase(str.length() - 2, 1);
+    cout << str;
     return 0;
 }
