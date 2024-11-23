9aec20d58e5036e76def1a16297de2b72d189f99
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-23 15:25:15 +0900

    Title: 대문자로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241123/대문자로 출력하기/print-in-capital.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+    string answer;
+    for (int i = 0; i < str.length(); i++) {
+        if (isalpha(str[i])) {
+            answer += (char) toupper(str[i]);
+        }
+    }
+    cout << answer;
+    return 0;
+}
