b5f77fe760f9e8c2e82bd98db827a30bf82a6d14
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-23 14:54:57 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241123/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for (int i = 0; i < 5; i++) {
+        int n;
+        cin >> n;
+        cout << (char) n << " ";
+    }
+    return 0;
+}
