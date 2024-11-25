a840265c48d2fac5ae5495d181879f0de9b57232
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-25 19:49:19 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 14ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

--- 241125/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.cpp
+++ 241125/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.cpp
@@ -5,14 +5,17 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    int arr[n];
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
+    int arr[50] = {};
+
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if (a % 2 == 0) arr[i] = a;
     }
-    for(int i = n; i >= 0; i--) {
-        if(arr[i] % 2 == 0 && arr[i] != 0) {
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (arr[i] != 0)
             cout << arr[i] << " ";
-        }
     }
     return 0;
 }
