b2c1703853f7791e28052f53125c444c513b799b
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-27 01:28:10 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241127/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 241127/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -19,7 +19,7 @@ bool IsLeapYear(int y) {
 bool IsLeapYear(int y) {
     if(y % 4 != 0) return false;
 
-    if(y % 100 != 0) return false;
+    if(y % 100 != 0) return true;
 
     if(y % 400 == 0) return true;
 
