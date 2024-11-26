3608522a8ee4801402a54df6a76d771bc7a990c5
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-27 01:27:02 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241127/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 241127/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+/* 내 코드
+중첩 조건문, 바로 return이 안됨.
 bool IsLeapYear(int y) {
     if(y % 4 == 0) {
         if(y % 100 == 0) {
@@ -11,6 +13,19 @@ bool IsLeapYear(int y) {
     }
     return false;
 }
+*/
+
+// 해답 코드
+bool IsLeapYear(int y) {
+    if(y % 4 != 0) return false;
+
+    if(y % 100 != 0) return false;
+
+    if(y % 400 == 0) return true;
+
+    if(y % 400 != 0) return false;
+}
+
 
 int main() {
     // 여기에 코드를 작성해주세요.
