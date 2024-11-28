3eb759ac88e37b7faf14b73476e18a89232464ef
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-28 16:51:43 +0900

    Title: 두 정수 값 교환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241128/두 정수 값 교환하기/to-exchange-two-integer-values.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void Swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    Swap(&n, &m);
+    cout << n << " " << m;
+    return 0;
+}
