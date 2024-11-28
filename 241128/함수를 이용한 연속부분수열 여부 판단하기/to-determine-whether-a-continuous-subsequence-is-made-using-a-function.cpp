f8355fc54f2f71d583f778b6bca120ed758f5dab
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-28 14:24:03 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241128/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 241128/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+int n1, n2;
+int a[100], b[100];
+
+/* 내 코드
 bool ConsecutiveNumSubSequence(int *a, int *b, int n1, int n2) {
     int cnt_a = 0, cnt_b = 0;
     for (int i = cnt_a; i < n1; i++) {
@@ -21,19 +25,34 @@ bool ConsecutiveNumSubSequence(int *a, int *b, int n1, int n2) {
     }
     return false;
 }
+*/
+
+bool IsSame(int n) {
+    for(int i = 0; i < n2; i++)
+        if(a[i + n] != b[i]) return false;
+    return true;
+}
+
+bool IsSubsequence() {
+    for(int i = 0; i <= n1 - n2; i++)
+        if(IsSame(i)) return true;
+    
+    return false;
+}
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n1, n2;
+    
     cin >> n1 >> n2;
-    int a[100], b[100];
+    
     for(int i = 0; i < n1; i++) {
         cin >> a[i];
     }
     for(int i = 0; i < n2; i++) {
         cin >> b[i];
     }
-    if(ConsecutiveNumSubSequence(a, b, n1, n2)) cout << "Yes";
+    // if(ConsecutiveNumSubSequence(a, b, n1, n2)) cout << "Yes";
+    if(IsSubsequence()) cout << "Yes";
     else cout << "No";
     return 0;
 }
