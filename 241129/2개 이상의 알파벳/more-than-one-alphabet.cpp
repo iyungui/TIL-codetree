3d96315c39584b359bb49b3304054bd8d20f1ae5
이융의 <121233350+iyungui@users.noreply.github.com>
2024-11-29 10:26:33 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241129/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string a;
+
+bool IsDifferent(string &a) {
+    string b;
+    int len = b.length();
+    for(int i = 0; i < a.length(); i++) {
+        if(b[len] != a[i]) b += a[i];
+        if(len >= 2) return true;
+    }
+    return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> a;
+    if(IsDifferent(a)) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
