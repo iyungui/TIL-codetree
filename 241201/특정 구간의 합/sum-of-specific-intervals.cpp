ba888afd041bc28cf5cf6e4e9d86168123742c14
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-01 21:42:25 +0900

    Title: 특정 구간의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241201/특정 구간의 합/sum-of-specific-intervals.cpp
+++ 241201/특정 구간의 합/sum-of-specific-intervals.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 #define MAX_VAL 101
 
-int a[MAX_VAL];
+int a[MAX_VAL + 1];
+
+int GetAnswer(int a1, int a2) {
+    int return_value = 0;
+    for(int i = a1; i <= a2; i++) {
+        return_value += a[i - 1];
+    }
+    return return_value;
+}
 
 int main() {
     int n, m;
@@ -12,15 +20,10 @@ int main() {
         cin >> a[i];
     }
 
-    int a1, a2;
-    
     for(int i = 0; i < m; i++) {
-        int sum = 0;
+        int a1, a2;
         cin >> a1 >> a2;
-        for(int j = a1; j <= a2; j++) {
-            sum += a[j - 1];
-        }
-        cout << sum << endl;
+        cout << GetAnswer(a1, a2) << endl;
     }
     return 0;
 }
