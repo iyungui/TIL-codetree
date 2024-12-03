721bf947bc9305e66bc026633395cba2bfa758cb
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-03 14:50:49 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 19ms, Memory: 1MB, Status: Passed - CodeTree

--- 241203/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
+++ 241203/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -3,21 +3,31 @@
 #include <algorithm>
 using namespace std;
 
-bool IsEqual(string a, string b) {
-    if(a.length() != b.length()) return false;
-    for(int i = 0; i < a.length(); i++)
-        if(a[i] != b[i]) return false;
-    return true;
-}
+// bool IsEqual(string a, string b) {
+//     if(a.length() != b.length()) return false;
+//     for(int i = 0; i < a.length(); i++)
+//         if(a[i] != b[i]) return false;
+//     return true;
+// }
+
+// int main() {
+//     // 여기에 코드를 작성해주세요.
+//     string a, b;
+//     cin >> a >> b;
+//     sort(a.begin(), a.end());
+//     sort(b.begin(), b.end());
+//     bool satisfied = IsEqual(a, b);
+//     if(satisfied) cout << "Yes";
+//     else cout << "No";
+//     return 0;
+// }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     string a, b;
     cin >> a >> b;
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    bool satisfied = IsEqual(a, b);
-    if(satisfied) cout << "Yes";
+    if(a.compare(b) == 0) cout << "Yes";
     else cout << "No";
     return 0;
 }
