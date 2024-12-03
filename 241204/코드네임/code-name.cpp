33f1dd60e37fd987621fe24d84a3e7eee9addbe3
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-04 00:42:48 +0900

    Title: 코드네임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241204/코드네임/code-name.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Agent {
+    public:
+        char codeName;
+        int score;
+        Agent(char codeName, int score) {
+            this->codeName = codeName;
+            this->score = score;
+        }
+        Agent() {};
+};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    Agent agents[5];
+    for(int i = 0; i < 5; i++) {
+        char codeName;
+        int score;
+        cin >> codeName >> score;
+        agents[i] = Agent(codeName, score);
+    }
+    int idx = 0;
+    int min_score = 0;
+    for(int i = 0; i < 5; i++) {
+        if(min_score > agents[i].score) {
+            min_score = agents[i].score;
+            idx = i;
+        }
+    }
+    // cout << idx;
+    cout << agents[idx].codeName << " " << agents[idx].score;
+    return 0;
+}
