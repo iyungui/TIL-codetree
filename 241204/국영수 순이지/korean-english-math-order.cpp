d3d998baba52a44bb5e9c15b7cf7766e4008885e
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-04 13:52:29 +0900

    Title: 국영수 순이지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241204/국영수 순이지/korean-english-math-order.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <tuple>
+using namespace std;
+
+class Student {
+    public:
+        string name;
+        int kor, eng, math;
+        Student(string name, int kor, int eng, int math) {
+            this->name = name;
+            this->kor = kor;
+            this->eng = eng;
+            this->math = math;
+        }
+        Student() {};
+};
+
+bool cmp(Student a, Student b) {
+    return make_tuple(a.kor, a.eng, a.math) > make_tuple(b.kor, b.eng, b.math);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    Student students[10];
+    for(int i = 0; i < n; i++) {
+        string name;
+        int kor, eng, math;
+        cin >> name >> kor >> eng >> math;
+        students[i] = Student(name, kor, eng, math);
+    }
+    sort(students, students + n, cmp);
+    for(int i = 0; i < n; i++) {
+        cout << students[i].name << " ";
+        cout << students[i].kor << " ";
+        cout << students[i].eng << " ";
+        cout << students[i].math << endl;
+    }
+    return 0;
+}
