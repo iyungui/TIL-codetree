5a91d6a0c2b5f2d6bbd5bc1a9f572f2aeee2ac3d
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-04 12:01:08 +0900

    Title: 키를 기준으로 정렬, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241204/키를 기준으로 정렬/sort-by-height.cpp
+++ 241204/키를 기준으로 정렬/sort-by-height.cpp
@@ -1,38 +1,68 @@
+// #include <iostream>
+// #include <string>
+// #include <algorithm>
+// using namespace std;
+
+// class Person {
+//     public:
+//         string name;
+//         int height, weight;
+//         Person(string name, int height, int weight) {
+//             this->name = name;
+//             this->height = height;
+//             this->weight = weight;
+//         }
+//         Person() {};
+// };
+
+// bool cmp(const Person &a, const Person &b) {
+//     return a.height < b.height;
+// }
+
+// int main() {
+//     // 여기에 코드를 작성해주세요.
+//     int n;
+//     cin >> n;
+//     Person people[10];
+//     for(int i = 0; i < n; i++) {
+//         string name;
+//         int height, weight;
+//         cin >> name >> height >> weight;
+//         people[i] = Person(name, height, weight);
+//     }
+//     sort(people, people + n, cmp);
+//     for(int i = 0; i < n; i++) {
+//         cout << people[i].name << " " << people[i].height << " " << people[i].weight << endl;
+//     }
+//     return 0;
+// }
+
+
+// Tuple
 #include <iostream>
-#include <string>
+#include <tuple>
 #include <algorithm>
+#include <string>
+
 using namespace std;
 
-class Person {
-    public:
-        string name;
-        int height, weight;
-        Person(string name, int height, int weight) {
-            this->name = name;
-            this->height = height;
-            this->weight = weight;
-        }
-        Person() {};
-};
-
-bool cmp(const Person &a, const Person &b) {
-    return a.height < b.height;
-}
-
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    Person people[10];
-    for(int i = 0; i < n; i++) {
+    tuple<int, string, int> people[10] = {};
+    for (int i = 0; i < n; i++) {
         string name;
         int height, weight;
         cin >> name >> height >> weight;
-        people[i] = Person(name, height, weight);
+        people[i] = make_tuple(height, name, weight);
     }
-    sort(people, people + n, cmp);
-    for(int i = 0; i < n; i++) {
-        cout << people[i].name << " " << people[i].height << " " << people[i].weight << endl;
+    sort(people, people + n);
+
+    for (int i = 0; i < n; i++) {
+        string name;
+        int height, weight;
+        tie(height, name, weight) = people[i];
+        cout << name << " " << height << " " << weight << endl;
     }
     return 0;
 }
