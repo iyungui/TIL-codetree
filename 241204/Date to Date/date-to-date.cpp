870d2b33cdd167056a459cfafb738cad0ffb0df0
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-04 22:09:27 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241204/Date to Date/date-to-date.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    // (1.1 ~ m2.d2) - (1.1 ~ m1.d1)
+    int m1, d1, m2, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int totalDays = 0;
+    int cnt = 1;
+    while(cnt < m2) {
+        if(m2 == 1) break;
+        totalDays += num_of_days[cnt];
+        cnt++;
+    }
+    totalDays += d2;
+
+    int firstDays = 0;
+    cnt = 1;
+    while(cnt < m1) {
+        if(m1 == 1) break;
+        firstDays += num_of_days[cnt];
+        cnt++;
+    }
+    firstDays += d1;
+
+    cout << totalDays - firstDays + 1;
+
+    return 0;
+}
