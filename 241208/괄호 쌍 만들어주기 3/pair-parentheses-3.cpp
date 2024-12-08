68a904c48cf0cee10dbb3128220316c0a127cdd0
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-08 11:45:02 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241208/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
+++ 241208/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -9,7 +9,7 @@ int main() {
     cin >> a;
     for(int i = 0; i < a.length(); i++) {
         if(a[i] == '(') {
-            for(int j = i; j < a.length(); j++) {
+            for(int j = i + 1; j < a.length(); j++) {
                 if(a[j] == ')')
                     cnt++;
             }
