c9068ed93269c416ee59c41f2fdfc2303b47ab03
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-10 23:34:16 +0900

    Title: G or H 3, Time: 19ms, Memory: 0MB, Status: Passed - CodeTree

--- 241210/G or H 3/G-or-H-3.cpp
+++ 241210/G or H 3/G-or-H-3.cpp
@@ -3,48 +3,72 @@
 using namespace std;
 #define MAX_N 10000
 
-int arr[MAX_N + 1] = {};
+// int arr[MAX_N + 1] = {};
 
-int GetMaxScore(int max_idx, int k) {
-    if(max_idx < k) {
-        int score = 0;
-        for(int i = 1; i <= max_idx; i++) {
-            score += arr[i];
-        }
-        return score;
-    }
+// int GetMaxScore(int max_idx, int k) {
+//     if(max_idx < k) {
+//         int score = 0;
+//         for(int i = 1; i <= max_idx; i++) {
+//             score += arr[i];
+//         }
+//         return score;
+//     }
+
+//     int max_score = INT_MIN;
+
+//     for(int i = 1; i <= max_idx - k; i++) {
+//         int score = 0;
+//         for(int j = i; j <= i + k; j++) {
+//             score += arr[j];
+//         }
+//         max_score = max(max_score, score);
+//     }
+
+//     return max_score;
+// }
+
+// int main() {
+//     // 여기에 코드를 작성해주세요.
+//     int n, k;
+//     cin >> n >> k;
     
-    int max_score = INT_MIN;
+//     int max_idx = INT_MIN;
 
-    for(int i = 1; i <= max_idx - k; i++) {
+//     for(int i = 1; i <= n; i++) {
+//         int idx;
+//         char a;
+//         cin >> idx;
+//         cin >> a;
+//         if(a == 'G') arr[idx] = 1;
+//         else if(a == 'H') arr[idx] = 2;
+
+//         max_idx = max(max_idx, idx);
+//     }
+
+//     cout << GetMaxScore(max_idx, k);
+//     return 0;
+// }
+
+int arr[MAX_N + 1];
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++) {
+        int idx;
+        char a;
+        cin >> idx >> a;
+        if(a == 'G') arr[idx] = 1;
+        else arr[idx] = 2;
+    }
+    int max_score = INT_MIN;
+    for(int i = 1; i <= MAX_N - k; i++) {
         int score = 0;
         for(int j = i; j <= i + k; j++) {
             score += arr[j];
         }
         max_score = max(max_score, score);
     }
-
-    return max_score;
-}
-
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int n, k;
-    cin >> n >> k;
-    
-    int max_idx = INT_MIN;
-
-    for(int i = 1; i <= n; i++) {
-        int idx;
-        char a;
-        cin >> idx;
-        cin >> a;
-        if(a == 'G') arr[idx] = 1;
-        else if(a == 'H') arr[idx] = 2;
-
-        max_idx = max(max_idx, idx);
-    }
-
-    cout << GetMaxScore(max_idx, k);
+    cout << max_score;
     return 0;
 }
