3501422f0b96e75e0db978cbf8444018588fc313
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-11 13:14:59 +0900

    Title: 개발자의 능력 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241211/개발자의 능력 3/ability-of-developer-3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <climits>
+using namespace std;
+
+int arr[6];
+
+int GetDiff(int i, int j, int k) {
+    int sum1 = arr[i] + arr[j] + arr[k];
+    int sum2 = 0;
+    for(int i = 0; i < 6; i++) sum2 += arr[i];
+    sum2 -= sum1;
+    return abs(sum1 - sum2);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int min_diff = INT_MAX;
+    for(int i = 0; i < 6; i++)
+        cin >> arr[i];
+    for(int i = 0; i < 6; i++) {
+        for(int j = i + 1; j < 6; j++) {
+            for(int k = j + 1; k < 6; k++) {
+                min_diff = min(min_diff, GetDiff(i, j, k));
+            }
+        }
+    }
+    cout << min_diff;
+    return 0;
+}
