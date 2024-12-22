2db6ebf9e19a32534b6fc4938158498338bba998
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-23 00:44:14 +0900

    Title: 벌금은 누구에게, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241223/벌금은 누구에게/who-will-pay.cpp
+++ 241223/벌금은 누구에게/who-will-pay.cpp
@@ -2,24 +2,21 @@
 using namespace std;
 
 #define MAX_N 100
+#define MAX_M 10000
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n, m, k;
-
     cin >> n >> m >> k;
+    int cnt_arr[MAX_M + 1];
+    int students[MAX_N + 1] = {0};
 
-    int arr[MAX_N];
-    for(int i = 0; i < m; i++) {
-        cin >> arr[i];
-    }
+    for(int i = 1; i <= m; i++) cin >> cnt_arr[i];
 
-    int students[MAX_N] = {0};
-
-    for(int i = 0; i < m; i++) {
-        students[arr[i]]++;
-        if(students[arr[i]] == k) {
-            cout << arr[i];
+    for(int i = 1; i <= m; i++) {
+        students[cnt_arr[i]]++;
+        if(students[cnt_arr[i]] == k) {
+            cout << cnt_arr[i];
             break;
         }
     }
