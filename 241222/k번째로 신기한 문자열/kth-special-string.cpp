fae4b5bf82f320d244ed3ee04d2c85827b07e909
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-22 14:05:58 +0900

    Title: k번째로 신기한 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241222/k번째로 신기한 문자열/kth-special-string.cpp
+++ 241222/k번째로 신기한 문자열/kth-special-string.cpp
@@ -12,14 +12,17 @@ string t_arr[MAX_N];
 
 int main() {
     cin >> n >> k >> t;
-    for(int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n; i++) cin >> arr[i];
 
-    sort(arr, arr + n);
+    sort(arr, arr + n);  // 사전순 정렬
+
     int cnt = 0;
-    for(int i = 0; i < n; i++) {
-        if (arr[i].substr(0, t.length()) == t)
-            t_arr[i] = arr[i];
+    for (int i = 0; i < n; i++) {
+        if (arr[i].substr(0, t.length()) == t) {
+            t_arr[cnt++] = arr[i];
+        }
     }
-    cout << t_arr[k];
+    cout << t_arr[k - 1] << endl;
+
     return 0;
 }
