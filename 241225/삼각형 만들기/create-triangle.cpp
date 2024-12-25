d62a14e7c6c54b83a3e2db92f763c239bb4aa3f7
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-25 18:31:49 +0900

    Title: 삼각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241225/삼각형 만들기/create-triangle.cpp
+++ 241225/삼각형 만들기/create-triangle.cpp
@@ -20,14 +20,14 @@ int main() {
         for(int j = i + 1; j < n; j++) {
             for(int k = j + 1; k < n; k++) {
                 if( (x[i] - x[j] == 0 || x[i] - x[k] == 0 || x[j] - x[k] == 0) && (y[i] - y[j] == 0 || y[i] - y[k] == 0 || y[j] - y[k] == 0) ) {
+                    
                     size = abs((x[i]*y[j] + x[j]*y[k] + x[k]*y[i]) - (x[j]*y[i] + x[k]*y[j] + x[i]*y[k]));
-                    size /= 2;
-
+                    if(size == 0) break;
                     ans = max(ans, size);
                 }
             }
         }
     }
-    cout << ans * 2;
+    cout << ans;
     return 0;
 }
