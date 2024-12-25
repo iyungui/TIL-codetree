0cb6886db624ae32e418291a357007cb16271bd7
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-26 00:33:27 +0900

    Title: 겹치지 않는 선분 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241226/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+#define MAX_N 100
+#define MAX_XX 2000000
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int x1[MAX_N], x2[MAX_N];
+
+    cin >> n;
+
+    int min_x = INT_MAX, max_x = INT_MIN;
+
+    for(int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+
+        min_x = min(min_x, x1[i]);
+        max_x = max(max_x, x2[i]);
+    }
+        
+    int countings[MAX_XX];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = x1[i]; j <= x2[i]; j++) {
+            countings[j]++;
+        }
+    }
+
+    int cnt = INT_MIN;
+    for(int i = min_x; i <= max_x; i++) {
+        cnt = max(cnt, countings[i]);
+    }
+
+    cout << n - cnt;
+    return 0;
+}
