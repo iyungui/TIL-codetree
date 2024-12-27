905bee853e74b570819890a62a6cd26c621303db
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-27 21:00:42 +0900

    Title: 개발자의 순위, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241227/개발자의 순위/developers-rank.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_K 10
+#define MAX_N 20
+
+int k, n;
+int arr[MAX_K + 1][MAX_N + 1];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> k >> n;
+
+    for(int i = 1; i <= k; i++) {
+        for(int j = 1; j <= n; j++) cin >> arr[i][j];
+    }
+
+    int cnt = 0;
+
+    for(int i = 1; i <= n; i++) {
+        int counting_arr[MAX_N + 1] = {0};
+        for(int j = 1; j <= k; j++) {
+            for(int l = i + 1; l <= n; l++) counting_arr[arr[j][l]]++;
+        }
+        for(int j = 1; j <= n; j++) {
+            if(counting_arr[j] == k) cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
