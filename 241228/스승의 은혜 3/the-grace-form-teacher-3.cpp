5f5c0a3fefefba2c28b20b5aef5f2e9c6a0a8c0e
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-28 14:02:54 +0900

    Title: 스승의 은혜 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241228/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 241228/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 #define MAX_N 1000
@@ -18,41 +19,63 @@ class Price {
 };
 
 int n, b;
-Price prices[MAX_N + 1];
+Price prices[MAX_N];
 
 bool Cmp(Price a, Price b) {
-    if(a.p + a.s == b.p + b.s) return a.p > b.p;
-    return a.p + a.s < b.p + b.s;
+    if(a.p + a.s == b.p + b.s) return a.p >= b.p;
+    return (a.p + a.s) < (b.p + b.s);
 }
 
 int main() {
     cin >> n >> b;
 
-    for(int i = 1; i <= n; i++) {
+    for(int i = 0; i < n; i++) {
         int p, s;
         cin >> p >> s;
 
         prices[i] = Price(p, s);
     }
 
-    sort(prices, prices + n + 1, Cmp);
+    sort(prices, prices + n, Cmp);
 
-    int ans = 0;
 
-    for(int i = 1; i <= n; i++) {
-        if(b - (prices[i].p + prices[i].s) > 0) {   // 해당 물건을 사고도 예산이 남아있다면 
-            // 구매
-            b -= prices[i].p + prices[i].s;
-            ans++;
+    
+    int ans = INT_MIN;
 
-        } else if(b - (prices[i].p + prices[i].s) <= 0) {   // 해당 물건을 사려고 할 때, 예산이 부족하거나 마지막(0원) 일 때
-            // 쿠폰 적용 (마지막으로 담을 물건에 적용)
-            if(b - ((prices[i].p / 2) + prices[i].s) >= 0) {
-                b -= prices[i].p / 2 + prices[i].s;
-                ans++;
-            } else break;
+    for(int i = 0; i < n; i++) {
+        int temp = b;
+        int cnt = 0;
+        prices[i].p /= 2;
+
+        for(int j = 0; j < n; j++) {
+            int total_p = prices[j].p + prices[j].s;
+            if(temp - total_p >= 0) {
+                temp -= total_p;
+                cnt++;
+            }
         }
+
+        ans = max(ans, cnt);
+        prices[i].p *= 2;
     }
+
+
+    // for(int i = 0; i < n; i++) {
+    //     if(b - (prices[i].p + prices[i].s) > 0) {   // 해당 물건을 사고도 예산이 남아있다면 
+    //         // 구매
+    //         b -= prices[i].p + prices[i].s;
+    //         ans++;
+
+    //     } else if(b - (prices[i].p + prices[i].s) <= 0) {   // 해당 물건을 사려고 할 때, 예산이 부족하거나 마지막(0원) 일 때
+    //         // 쿠폰 적용 (마지막으로 담을 물건에 적용)
+    //         if(b - ((prices[i].p / 2) + prices[i].s) >= 0) {
+    //             b -= prices[i].p / 2 + prices[i].s;
+    //             ans++;
+    //         } else break;
+    //     }
+    // }
+
+
     cout << ans;
     return 0;
 }
