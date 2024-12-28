c7961614e3bab6f22e0f3539f8f98a192aafb567
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-28 12:28:58 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241228/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 1000
+
+class Price {
+    public:
+        int p;
+        int s;
+
+        Price(int p, int s) {
+            this->p = p;
+            this->s = s;
+        }
+
+        Price() {};
+};
+
+int n, b;
+Price prices[MAX_N + 1];
+
+bool Cmp(Price a, Price b) {
+    return a.p + a.s < b.p + b.s;
+}
+
+int main() {
+    cin >> n >> b;
+
+    for(int i = 1; i <= n; i++) {
+        int p, s;
+        cin >> p >> s;
+
+        prices[i] = Price(p, s);
+    }
+
+    sort(prices, prices + n + 1, Cmp);
+
+    int ans = 0;
+
+    for(int i = 1; i <= n; i++) {
+        if(b - (prices[i].p + prices[i].s) > 0) {   // 해당 물건을 사고도 예산이 남아있다면 
+            // 구매
+            b -= prices[i].p + prices[i].s;
+            ans++;
+        } else if(b - (prices[i].p + prices[i].s) <= 0) {   // 해당 물건을 사려고 할 때, 예산이 부족하거나 마지막(0원) 일 때
+            // 쿠폰 적용 (마지막으로 담을 물건에 적용)
+            if(b - (prices[i].p / 2) >= 0) {
+                b -= prices[i].p / 2 + prices[i].s;
+                ans++;
+            } else break;
+        }
+    }
+    cout << ans;
+    return 0;
+}
