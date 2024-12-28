2b5c0b21f14933b0347819a7ec76401e48963772
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-28 14:55:30 +0900

    Title: 숫자들의 합 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241228/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
+++ 241228/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -3,20 +3,29 @@ using namespace std;
 
 int x, y;
 
+int DigitSum(int n) {
+    if(n < 10) return n;
+
+    return DigitSum(n / 10) + (n % 10);
+}
+
 int main() {
     cin >> x >> y;
     int ans = 0;
     
+    // for(int i = x; i <= y; i++) {
+
+    //     int d1 = i % 10;
+    //     int d2 = (i / 10) % 10;
+    //     int d3 = (i / 100) % 10;
+    //     int d4 = (i / 1000) % 10;
+
+    //     ans = max(ans, d1 + d2 + d3 + d4);
+    // }
+
     for(int i = x; i <= y; i++) {
-
-        int d1 = i % 10;
-        int d2 = (i / 10) % 10;
-        int d3 = (i / 100) % 10;
-        int d4 = (i / 1000) % 10;
-
-        ans = max(ans, d1 + d2 + d3 + d4);
+        ans = max(ans, DigitSum(i));
     }
-
     cout << ans;
     return 0;
 }
