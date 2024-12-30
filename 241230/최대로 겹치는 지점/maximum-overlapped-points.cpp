6a51f23b10e83be97484ecf23e5d2a7f64951af0
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-30 19:51:14 +0900

    Title: 최대로 겹치는 지점, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241230/최대로 겹치는 지점/maximum-overlapped-points.cpp
+++ 241230/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 #define MAX_N 100
 
 int n;
-int countings[MAX_N];
-int arr[MAX_N][2];
+int countings[MAX_N + 1];
+int arr[MAX_N + 1][2];
 
 int main() {
     cin >> n;
 
-    for(int i = 0; i < n; i++)
+    for(int i = 1; i <= n; i++)
         cin >> arr[i][0] >> arr[i][1];
     
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         int x = arr[i][0], y = arr[i][1];
 
         for(int j = x; j <= y; j++) countings[j]++;
     }
 
     int max_cnt = 0;
-    for(int i = 1; i <= 100; i++) max_cnt = max(max_cnt, countings[i]);
+    for(int i = 1; i <= 100; i++) {
+        if(countings[i] > max_cnt) max_cnt = countings[i];
+    }
     cout << max_cnt;
     return 0;
 }
