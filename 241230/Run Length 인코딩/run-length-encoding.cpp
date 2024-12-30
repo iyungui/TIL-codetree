bbc289198486abdfd1192735803edfe16670380e
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-30 17:05:30 +0900

    Title: Run Length 인코딩, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241230/Run Length 인코딩/run-length-encoding.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string a;
+
+int main() {
+    cin >> a;
+    int cnt = 1;
+    int len = 0;
+
+    string new_a;
+
+    for(int i = 1; i < a.length(); i++) {
+        if(a[i - 1] == a[i]) {
+            cnt++;
+        } else if (a[i - 1] != a[i]) {
+            new_a += a[i - 1];
+            char after = cnt + '0';
+            new_a += after;
+            cnt = 1;
+            len += 2;
+        }
+
+        if(i == a.length() - 1) {
+            // cout << a[i - 1] << cnt;
+            new_a += a[i - 1];
+            char after = cnt + '0';
+            new_a += after;
+
+            len += 2;
+        }
+    }
+
+    cout << len << endl;
+
+    for(int i = 0; i < len; i++) {
+        cout << new_a[i];
+    }
+
+    return 0;
+}
