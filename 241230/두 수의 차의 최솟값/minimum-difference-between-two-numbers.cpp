8478cba76d11143d81bae56fbbe05074a001faaa
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-30 15:43:50 +0900

    Title: 두 수의 차의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241230/두 수의 차의 최솟값/minimum-difference-between-two-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#define MAX_N 10
+
+int n;
+int arr[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    int min_val = 10;
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++)
+            if(min_val > (abs(arr[i] - arr[j]))) min_val = abs(arr[i] - arr[j]); 
+    }
+
+    cout << min_val;
+    return 0;
+}
