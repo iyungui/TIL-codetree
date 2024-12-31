2c0ad01d89a6f0ef2f2c136c52323d553d419d6f
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-31 11:22:01 +0900

    Title: 사각형의 총 넓이 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241231/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 241231/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -1,42 +1,40 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-#define MAX_SIZE 201  // -100 ~ 100 범위를 양수로 변환하면 0~200
+#define MAX_N 10
+#define MAX_SIZE 200
+#define OFFSET 100
 
 int n;
-int grid[MAX_SIZE][MAX_SIZE];  // 영역을 표시할 2차원 배열
+int x1[MAX_N], y1[MAX_N], x2[MAX_N], y2[MAX_N];
+int grid[MAX_SIZE + 1][MAX_SIZE + 1];
 
 int main() {
     cin >> n;
-    
-    // 좌표를 입력받아 직사각형 영역을 표시
+
     for(int i = 0; i < n; i++) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-        
-        // 좌표를 양수로 변환 (음수 좌표 처리를 위해 +100)
-        x1 += 100;
-        y1 += 100;
-        x2 += 100;
-        y2 += 100;
-        
-        // 직사각형 영역을 1로 표시
-        for(int x = x1; x < x2; x++) {
-            for(int y = y1; y < y2; y++) {
-                grid[x][y] = 1;
-            }
+        cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+        x1[i] += OFFSET;
+        y1[i] += OFFSET;
+        x2[i] += OFFSET;
+        y2[i] += OFFSET;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = x1[i]; j < x2[i]; j++) {
+            for(int k = y1[i]; k < y2[i]; k++)
+                grid[j][k] = 1;
         }
     }
-    
-    // 1로 표시된 영역의 개수를 세어 총 넓이 계산
+
     int area = 0;
-    for(int i = 0; i < MAX_SIZE; i++) {
-        for(int j = 0; j < MAX_SIZE; j++) {
+    for(int i = 0; i <= MAX_SIZE; i++) {
+        for(int j = 0; j <= MAX_SIZE; j++) {
             if(grid[i][j] == 1) area++;
         }
     }
-    
+
     cout << area;
     return 0;
 }
