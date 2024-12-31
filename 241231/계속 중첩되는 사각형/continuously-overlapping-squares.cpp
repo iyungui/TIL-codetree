79b1bba656c3a58e13b9ecd8b9304d13676a54de
이융의 <121233350+iyungui@users.noreply.github.com>
2024-12-31 15:01:32 +0900

    Title: 계속 중첩되는 사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241231/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_R 200
+#define N 10
+#define OFFSET 100
+
+int x1[N + 1], y1[N + 1], x2[N + 1], y2[N + 1];
+int checked[MAX_R + 1][MAX_R + 1];
+int n;
+
+int main() {
+    
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+
+        x1[i] += OFFSET;
+        y1[i] += OFFSET;
+        x2[i] += OFFSET;
+        y2[i] += OFFSET;
+    }
+
+    bool IsRed = true;
+
+    for(int i = 0; i < n; i++) {
+        for(int x = x1[i]; x < x2[i]; x++) {
+            for(int y = y1[i]; y < y2[i]; y++) {
+                if(IsRed) checked[x][y] = 1;
+                else checked[x][y] = 2;
+            }
+        }
+
+        if(IsRed) IsRed = false;
+        else IsRed = true;
+    }
+
+    int area = 0;
+
+    for(int x = 0; x <= MAX_R; x++)
+        for(int y = 0; y <= MAX_R; y++)
+            if(checked[x][y] == 2) area++;
+    
+    cout << area;
+
+    return 0;
+}
