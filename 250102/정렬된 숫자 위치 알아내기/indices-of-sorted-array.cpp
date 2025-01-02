1d34daa14b088a8ba2819cab80f44f921caf025a
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-02 10:40:59 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250102/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 1000
+
+class Seq {
+    public:
+        int x, num, idx;
+        // idx 기본값 설정 방법?
+        Seq(int x, int num, int idx) {
+            this->x = x;
+            this->num = num;
+            this->idx = idx;
+        }
+
+        Seq() {};
+};
+
+int n;
+Seq seqs[MAX_N];
+
+bool Cmp_x(Seq a, Seq b) {
+    return a.x < b.x;
+}
+
+bool Cmp_num(Seq a, Seq b) {
+    return a.num < b.num;
+}
+
+int main() {
+    cin >> n;
+
+    int x, num, idx;
+    for(int i = 0; i < n; i++) {
+        
+        cin >> x;
+
+        seqs[i] = Seq(x, i + 1, i + 1);
+    }
+
+    sort(seqs, seqs + n, Cmp_x);
+
+    for(int i = 0; i < n; i++) {
+        seqs[i].idx = i + 1;
+    }
+
+    sort(seqs, seqs + n, Cmp_num);
+
+    for(int i = 0; i < n; i++) {
+        cout << seqs[i].idx << " ";
+    }
+    return 0;
+}
