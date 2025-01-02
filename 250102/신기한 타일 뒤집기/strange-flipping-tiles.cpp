4dd3044b2e5ba3a03409bdfd6524588d59c01a88
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-02 17:36:53 +0900

    Title: 신기한 타일 뒤집기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250102/신기한 타일 뒤집기/strange-flipping-tiles.cpp
+++ 250102/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -1,48 +1,40 @@
 #include <iostream>
 using namespace std;
 
-#define MAX_N 1000
-#define MAX_X 100
-#define OFFSET 10000  // 충분히 큰 오프셋 값 설정
+#define MAX_R 200000
+#define OFFSET 100000
 
 int n;
-// 0: 회색(초기값), 1: 흰색(L), 2: 검은색(R)
-int tiles[OFFSET * 2 + 1];  // 음수 인덱스를 위해 충분히 큰 배열 선언
+int checked[MAX_R + 1];
+int cur = 0;
 
 int main() {
     cin >> n;
-    
-    // 현재 위치 (오프셋을 더해서 시작)
-    int curr_pos = OFFSET;
-    
-    // n번의 명령 처리
     for(int i = 0; i < n; i++) {
-        int x;
+        int distance;
         char direction;
-        cin >> x >> direction;
-        
-        if(direction == 'L') {
-            // 왼쪽으로 x칸 이동하며 흰색으로 변경
-            for(int j = 0; j < x; j++) {
-                tiles[curr_pos - j] = 1;  // 흰색으로 변경
+        cin >> distance >> direction;
+
+        if(direction == 'R') {
+            // 현재 위치부터 distance개의 타일을 뒤집음
+            for(int j = 0; j < distance; j++) {
+                checked[cur + OFFSET + j] = 2;
             }
-            curr_pos -= (x - 1);  // 마지막 위치로 이동
-        } else {  // direction == 'R'
-            // 오른쪽으로 x칸 이동하며 검은색으로 변경
-            for(int j = 0; j < x; j++) {
-                tiles[curr_pos + j] = 2;  // 검은색으로 변경
+            cur += (distance - 1);  // 마지막으로 뒤집은 타일의 위치로
+        } else {  // direction == 'L'
+            // 현재 위치부터 왼쪽으로 distance개의 타일을 뒤집음
+            for(int j = 0; j < distance; j++) {
+                checked[cur + OFFSET - j] = 1;
             }
-            curr_pos += (x - 1);  // 마지막 위치로 이동
+            cur -= (distance - 1);  // 마지막으로 뒤집은 타일의 위치로
         }
     }
-    
-    // 흰색과 검은색 타일 개수 세기
-    int white = 0, black = 0;
-    for(int i = 0; i <= OFFSET * 2; i++) {
-        if(tiles[i] == 1) white++;
-        else if(tiles[i] == 2) black++;
+
+    int cnt_w = 0, cnt_b = 0;
+    for(int i = 0; i <= MAX_R; i++) {
+        if(checked[i] == 1) cnt_w++;
+        else if(checked[i] == 2) cnt_b++;
     }
-    
-    cout << white << " " << black;
+    cout << cnt_w << " " << cnt_b;
     return 0;
 }
