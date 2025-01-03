a32ee265aa9780940db53596f9956f42d382c073
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-03 15:53:49 +0900

    Title: 거울에 레이저 쏘기 2, Time: 1050ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 250103/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 250103/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -1,102 +1,63 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 #define MAX_N 1000
 
 int n;
-
 char arr[MAX_N + 1][MAX_N + 1]; // 1 based Indexing
 
 int k;
+int x, y, move_dir;
 
-int getStartRow() {
-    if(k <= n) return 1;  // 상단
-    else if(k <= 2*n) return k - n;  // 우측
-    else if(k <= 3*n) return n;  // 하단
-    else return 4*n - k + 1;  // 좌측
-}
-
-int getStartCol() {
-    if(k <= n) return k;  // 상단
-    else if(k <= 2*n) return n;  // 우측
-    else if(k <= 3*n) return 3*n - k + 1;  // 하단 (역순)
-    else return 1;  // 좌측
-}
-
-int GetDir() {
-    if(k >= 1 && k <= n) return 0;  // 아래
-    if(k >= n + 1 && k <= 2 * n) return 1;  // 왼쪽
-    if(k >= 2 * n + 1 && k <= 3 * n) return 2;  // 위쪽
-    if(k >= 3 * n + 1 && k <= 4 * n) return 3;  // 오른쪽 
+void Initialize(int num) {
+    if(num <= n) {
+        x = 1; y = num; move_dir = 0;
+    } else if(num <= 2 * n) {
+        x = num - n - 1; y = n - 1, move_dir = 1; 
+    } else if(num <= 3 * n) {
+        x = n - 1; y = n - (num - 2 * n); move_dir = 2;
+    } else {
+        x = n - (num - 3 * n); y = 0; move_dir = 3;
+    }
 }
 
 bool InRange(int x, int y) {
     return x >= 1 && x <= n && y >= 1 && y <= n;
 }
 
-int Rotate(int x, int y, int curr_dir, char curr_mirror) {
-    if(curr_mirror == '/') {
-        if(curr_dir == 0) return 1;
-        if(curr_dir == 1) return 0;
-        if(curr_dir == 2) return 3;
-        if(curr_dir == 3) return 2;
-    } else {
-        if(curr_dir == 0) return 3;
-        if(curr_dir == 1) return 2;
-        if(curr_dir == 2) return 1;
-        if(curr_dir == 3) return 0;
-    }
+void Move(int next_dir) {
+    // 아래, 왼, 위, 오른쪽 순
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, -1, 0, 1};
+
+    x += dx[next_dir];
+    y += dy[next_dir];
+    move_dir = next_dir;
 }
 
-    // 아래, 왼, 위, 오른쪽 순
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, -1, 0, 1};
+int Simulate() {
+    int move_num = 0;
+    while(InRange(x, y)) {
+        if(arr[x][y] == '/') Move(move_dir ^ 1);
+        else Move(3 - move_dir);
+        move_num += 1;
+    }
+
+    return move_num;
+}
 
 int main() {
-    // 입력
     cin >> n;
 
-    string line;
-
-    for(int i = 1; i <= n; i++) {
-        cin >> line;
-
-        for(int j = 1; j <= n; j++) {
-            arr[i][j] = line[j - 1];
-        }
-    }
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            cin >> arr[i][j];
 
     cin >> k;
 
-    // 출발지점
-    int row = getStartRow();    // x
-    int col = getStartCol();    // y
-    // cout << " 출발지점: " << row << " " << col << endl;
-    // 출발 방향
-    int curr_dir = GetDir();
+    Initialize(k);
 
-    int nx, ny;
-
-    curr_dir = Rotate(row, col, curr_dir, arr[row][col]);
-    int ans = 1;
-    while(true) {
-        
-        nx = row + dx[curr_dir];
-        ny = col + dy[curr_dir];
-
-        if(!InRange(nx, ny)) {
-            break;
-        }
-
-        char next_mirror = arr[nx][ny];
-        curr_dir = Rotate(nx, ny, curr_dir, next_mirror);
-        ans++;
-
-        // cout << next_mirror << " " << ans << " ";
-        row = nx;
-        col = ny;
-    }
+    int ans = Simulate();
 
     cout << ans;
     return 0;
