e54aab38dc066e7ddf484e9cf9e2f4db6117671c
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-03 17:41:34 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250103/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 250103/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -1,13 +1,60 @@
 #include <iostream>
 using namespace std;
 
+// #define MAX_N 100
+
+// int n;
+// int arr[MAX_N][MAX_N];
+// int dx[4] = {0, 1, 0, -1};
+// int dy[4] = {1, 0, -1, 0};
+// int curr_dir = 2;
+
+// bool InRange(int x, int y) {
+//     return x >= 0 && x < n && y >= 0 && y < n;
+// }
+
+// int main() {
+//     cin >> n;
+//     int x = n - 1, y = n - 1;   // current position
+
+//     arr[x][y] = n * n;
+
+//     int nx, ny;
+//     for(int i = n * n - 1; i >= 1; i--) {
+//         nx = x + dx[curr_dir];
+//         ny = y + dy[curr_dir];
+
+//         if(!InRange(nx,ny) || arr[nx][ny] != 0)
+//             curr_dir = (curr_dir + 1) % 4;
+        
+//         x += dx[curr_dir];
+//         y += dy[curr_dir];
+
+//         arr[x][y] = i;
+//     }
+
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = 0; j < n; j++)
+    //         cout << arr[i][j] << " ";
+    //     cout << endl;
+    // }
+    // return 0;
+// }
+
+
+// 해설 코드
+
 #define MAX_N 100
 
 int n;
+
 int arr[MAX_N][MAX_N];
-int dx[4] = {0, 1, 0, -1};
+int dx[4] = {0, -1, 0, 1};
 int dy[4] = {1, 0, -1, 0};
-int curr_dir = 2;
+int curr_dir = 0;
+
+// 이동할 횟수
+int move_num = 1;
 
 bool InRange(int x, int y) {
     return x >= 0 && x < n && y >= 0 && y < n;
@@ -15,22 +62,26 @@ bool InRange(int x, int y) {
 
 int main() {
     cin >> n;
-    int x = n - 1, y = n - 1;   // current position
 
-    arr[x][y] = n * n;
+    int x = n / 2, y = n / 2;   // current position
+    
+    int cnt = 1;
 
     int nx, ny;
-    for(int i = n * n - 1; i >= 1; i--) {
-        nx = x + dx[curr_dir];
-        ny = y + dy[curr_dir];
+    while(InRange(x, y)) {
+        // move_num 만큼 이동
+        for(int i = 0; i < move_num; i++) {
+            arr[x][y] = cnt++;
 
-        if(!InRange(nx,ny) || arr[nx][ny] != 0)
-            curr_dir = (curr_dir + 1) % 4;
-        
-        x += dx[curr_dir];
-        y += dy[curr_dir];
+            x += dx[curr_dir];
+            y += dy[curr_dir];
 
-        arr[x][y] = i;
+            if(!InRange(x, y)) break;
+        }
+
+        curr_dir = (curr_dir + 1) % 4;
+
+        if(curr_dir == 0 || curr_dir == 2) move_num++;
     }
 
     for(int i = 0; i < n; i++) {
