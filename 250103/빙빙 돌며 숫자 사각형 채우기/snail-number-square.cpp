e2d9e435b712f5e2e0694ef35ec64dd3c2fe76dd
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-03 10:41:33 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250103/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 250103/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define MAX_NM 100
 
 int n, m;
-int answer[MAX_NM][MAX_NM]; // 0 based Indexing
+int arr[MAX_NM][MAX_NM]; // 0 based Indexing
 
 bool InRange(int x, int y) {
     return x >= 0 && x < n && y >= 0 && y < m;
@@ -14,32 +14,31 @@ int main() {
     cin >> n >> m;
 
     // 출발 지점
-    answer[0][0] = 1;
-
-    int cnt = 2;
+    arr[0][0] = 1;
     int x = 0, y = 0;
     int dx[4] = {0, 1, 0, -1};
     int dy[4] = {1, 0, -1, 0};
     int curr_dir = 0;
 
     int nx, ny;
+
+    // i는 2부터
     for(int i = 2; i <= n * m; i++) {
         nx = x + dx[curr_dir];
         ny = y + dy[curr_dir];
 
         // 벗어났다면, 혹은 채워져있다면
-        if(!InRange(nx, ny) || answer[nx][ny] != 0)
+        if(!InRange(nx, ny) || arr[nx][ny] != 0)
             curr_dir = (curr_dir + 1) % 4;
 
         x = x + dx[curr_dir];
         y = y + dy[curr_dir];
-        answer[x][y] = cnt++;
+        arr[x][y] = i;
     }
 
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cout << answer[i][j] << " ";
-        }
+        for(int j = 0; j < m; j++)
+            cout << arr[i][j] << " ";
         cout << endl;
     }
     return 0;
