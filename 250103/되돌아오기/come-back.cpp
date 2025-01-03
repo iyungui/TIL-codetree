9f98bb7b5e05f878408c6319ef0ca4d3baede1c5
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-03 11:53:15 +0900

    Title: 되돌아오기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250103/되돌아오기/come-back.cpp
+++ 250103/되돌아오기/come-back.cpp
@@ -8,6 +8,7 @@ int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, -1, 0, 1};
 char c;
 int d;
+int answer = -1;
 
 int GetDir(char a) {
     if(a == 'E') return 0;
@@ -16,32 +17,28 @@ int GetDir(char a) {
     if(a == 'N') return 3;
 }
 
+bool Move(int curr_dir, int distance) {
+    while(distance--) {
+        x += dx[curr_dir];
+        y += dy[curr_dir];
+        t++;
+        if(x == 0 && y == 0) {
+            answer = t;
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
     cin >> n;
 
-    int answer = -1;
-    int nx, ny;
     for(int i = 0; i < n; i++) {
         cin >> c >> d;
 
         int curr_dir = GetDir(c);
-
-        for(int j = 0; j < d; j++) {
-            nx = x + dx[curr_dir];
-            ny = y + dy[curr_dir];
-
-            t++;
-
-            if(nx == 0 && ny == 0) {
-                answer = t;
-                break;
-            }
-            x = nx;
-            y = ny;
-        }
-
-        if(answer == t) break; 
+        
+        if(Move(curr_dir, d)) break; 
     }
 
     cout << answer;
