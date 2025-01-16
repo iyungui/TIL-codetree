1c97538ad6f2ff46804626efadf9506ccba8de77
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-16 22:14:07 +0900

    Title: 아름다운 수열 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250116/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 250116/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -2,32 +2,39 @@
 using namespace std;
 
 int n, m, ret;
-vector<int> b;
+vector<int> a, b;
 
-bool check(vector<int>& window) {
-    vector<int> temp = b;
-    do {
-        if(window == temp) return true;
-    } while(next_permutation(temp.begin(), temp.end()));
-    return false;
+bool check_permutation(vector<int>& window, vector<int>& target) {
+    vector<int> temp = window;
+    sort(temp.begin(), temp.end());
+    return temp == target;
 }
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
     cin >> n >> m;
     
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
-    
-    b.resize(m);
-    for(int i = 0; i < m; i++) cin >> b[i];
-    sort(b.begin(), b.end()); // next_permutation을 위해 정렬 필수!
-    
-    for(int i = 0; i <= n - m; i++) {
-        vector<int> window(a.begin() + i, a.begin() + i + m);
-        if(check(window)) ret++;
+    // 수열 A 입력
+    int temp;
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        a.push_back(temp);
     }
     
-    cout << ret << "\n";
+    // 수열 B 입력
+    for(int i = 0; i < m; i++) {
+        cin >> temp;
+        b.push_back(temp);
+    }
+    
+    // 수열 B 정렬 (비교를 위해)
+    sort(b.begin(), b.end());
+    
+    // 슬라이딩 윈도우로 확인
+    for(int i = 0; i <= n - m; i++) {
+        vector<int> window(a.begin() + i, a.begin() + i + m);
+        if(check_permutation(window, b)) ret++;
+    }
+    
+    cout << ret << '\n';
     return 0;
 }
