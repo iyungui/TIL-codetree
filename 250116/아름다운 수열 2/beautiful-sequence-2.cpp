14b03488be3d960f77494d8cde76235450bf63f0
이융의 <121233350+iyungui@users.noreply.github.com>
2025-01-16 22:13:00 +0900

    Title: 아름다운 수열 2, Time: 1074ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 250116/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 250116/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -1,31 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, ret, a[104], temp;
-string s;
-
+int n, m, ret; 
+vector<int> a, sub_a, b;
 
 int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
     cin >> n >> m;
+    
+    // 수열 A 입력
     for(int i = 0; i < n; i++) {
-        cin >> temp;
-        s += (char)temp + '0';
+        int x; cin >> x;
+        a.push_back(x);
     }
+    
+    // 수열 B 입력
     for(int i = 0; i < m; i++) {
-        cin >> a[i];
+        int x; cin >> x;
+        b.push_back(x);
     }
-
-    sort(a, a + m);
-
-    do {
-        string b;
-        for(int i = 0; i < m; i++) {
-            b += (char)a[i] + '0';
+    sort(b.begin(), b.end()); // next_permutation 사용을 위한 정렬
+    
+    // 각 연속 부분수열 체크
+    for(int i = 0; i <= n - m; i++) {
+        sub_a = vector<int>(a.begin() + i, a.begin() + i + m);
+        sort(sub_a.begin(), sub_a.end());
+        
+        if(sub_a == b) ret++;
+        while(next_permutation(b.begin(), b.end())){
+            if(sub_a == b) ret++;
         }
-        if(s.find(b) != string::npos) ret++;
-
-    } while(next_permutation(a, a + m));
-
+        sort(b.begin(), b.end()); // b 다시 정렬
+    }
+    
     cout << ret << '\n';
     return 0;
 }
