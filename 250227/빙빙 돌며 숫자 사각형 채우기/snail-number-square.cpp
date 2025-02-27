fd90adfc5781731815a561199761990d30e02df0
이융의 <121233350+iyungui@users.noreply.github.com>
2025-02-27 22:04:27 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250227/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[100][100];
+const int dy[] = {0, 1, 0, -1};
+const int dx[] = {1, 0, -1, 0};
+
+int y, x;
+int dir;    // 0: 오른쪽, 1: 아래, 2: 왼, 3: 위
+
+int main() {
+    cin >> n >> m;
+    
+    for (int i = 1; i <= n * m; i++) {
+        arr[y][x] = i;
+        if (i == n * m) {
+            break;
+        }
+        int ny = y + dy[dir], nx = x + dx[dir];
+        if(ny >= n || ny < 0 || nx >= m || nx < 0 || arr[ny][nx] != 0)
+            dir = (dir + 1) % 4;    // 더 이상 나아갈 수 없을 때, 시계방향 회전
+
+        y += dy[dir], x += dx[dir];
+        
+    }
+
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
