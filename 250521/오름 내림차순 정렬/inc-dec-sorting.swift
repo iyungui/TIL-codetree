b0ac8ee14c6776af42b3c64287de746adb0fed0e
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-21 15:12:00 +0900

    Title: 오름 내림차순 정렬, Time: 23ms, Memory: 3MB, Status: Runtime Error - Codetree

--- 250521/오름 내림차순 정렬/inc-dec-sorting.swift
+++ 250521/오름 내림차순 정렬/inc-dec-sorting.swift
@@ -2,5 +2,9 @@ import Foundation
 
 let n = Int(readLine()!)!
 let input = readLine()!.components(separatedBy: " ").map { Int($0)! }
-print(input.sorted(by: <).map { String($0) }.joined(separator: " "))
+
+// 오름차순 정렬 결과 출력
+print(input.sorted().map { String($0) }.joined(separator: " "))
+
+// 내림차순 정렬 결과 출력
 print(input.sorted(by: >).map { String($0) }.joined(separator: " "))
